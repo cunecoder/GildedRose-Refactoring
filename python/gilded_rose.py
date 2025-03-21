a742260ae4ead882adcf83193ba0d1719318b0af
David Marin <david.marin@student.cune.edu>
2025-03-20 23:12:16 -0500

    Commit after texttest 30-day testcase passed :)

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -50,52 +50,6 @@ class GildedRose(object):
                 elif item.quality > 0:
                     item.quality -= 1
 
-    
-
-
-
-    # def update_quality(self):
-    #     for item in self.items:
-    #         # If item isn't brie, backstage pass, or sulfuras AND item quality > 0, subtract 1 quality
-    #         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-    #             if item.quality > 0:
-    #                 if item.name != "Sulfuras, Hand of Ragnaros":
-    #                     # Take first quality off this day
-    #                     item.quality = item.quality - 1
-    #         # Otherwise, if pass, 
-    #         else:
-    #             if item.quality < 50:
-    #                 # Increase bri and stage pass
-    #                 item.quality = item.quality + 1
-    #                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
-    #                     # Increase stage (total=2) pass if 10<days but not over 50 
-    #                     if item.sell_in < 11:
-    #                         if item.quality < 50:
-    #                             item.quality = item.quality + 1
-    #                     # Incrase stage (total=3) pass if 5<days but not over 50
-    #                     if item.sell_in < 6:
-    #                         if item.quality < 50:
-    #                             item.quality = item.quality + 1
-    #         # All items have one less day to sell by, not sulfuras
-    #         if item.name != "Sulfuras, Hand of Ragnaros":
-    #             item.sell_in = item.sell_in - 1
-
-    #         # If past sell time
-    #         if item.sell_in < 0:
-    #             if item.name != "Aged Brie":
-    #                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
-    #                     # NORMAL ITEM If sell_in < 0, take an extra (total=2) quality off
-    #                     if item.quality > 0:
-    #                         if item.name != "Sulfuras, Hand of Ragnaros":
-    #                             item.quality = item.quality - 1
-    #                 else:
-    #                     # If backstage pass, item quality = 0
-    #                     item.quality = item.quality - item.quality
-    #             else:
-    #                 # If brie and quality !>50, increase quality again (total=2)
-    #                 if item.quality < 50:
-    #                     item.quality = item.quality + 1
-
 
 class Item:
     def __init__(self, name, sell_in, quality):
